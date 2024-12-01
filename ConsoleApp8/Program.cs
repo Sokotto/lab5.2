bd61a30175fd0ee9e8dd366911d192b04dad49ba
Компьютер <@DESKTOP-RDMTMQN>
2024-12-01 15:32:01 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+string transformedString = TransformString(input);
+Console.WriteLine("Преобразованная строка: " + transformedString);
+static string TransformString(string input)
+{
+    var result = new System.Text.StringBuilder();
+    foreach (char c in input)
+    {
+        result.Append(c);
+        result.Append((int)c);
+    }
+    return result.ToString();
+}
